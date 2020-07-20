0f547ce5f578ea4376c89bc094fd2131b415977a
cocoball02 <seokho9522@gmail.com>
2020-07-20 12:24:44 +0900

    Core/Database Setting
    added get, post method in controllers

---
+++ models/order_lists.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class order_lists extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  order_lists.init({
+    user_id: DataTypes.INTEGER,
+    goods_id: DataTypes.INTEGER,
+    goods_quantity: DataTypes.INTEGER,
+    order_date: DataTypes.STRING,
+    rec_name: DataTypes.STRING,
+    rec_phone: DataTypes.STRING,
+    rec_address: DataTypes.STRING,
+    invoice_number: DataTypes.INTEGER,
+    order_state: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'order_lists',
+  });
+  return order_lists;
+};
