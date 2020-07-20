b2980ade0810e234210aa23c8574f2a1b29f8a8a
cocoball02 <seokho9522@gmail.com>
2020-07-20 11:28:25 +0900

    Core / server-setting

---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  homeController: require("./home/home"),
+  userController: require("./user"),
+  mypageController: require("./mypage"),
+  goodsController: require("./goods"),
+};
