db7c501df7278d885a2818c3cd8908f39909c356
coco_dev <57708971+cocoball02@users.noreply.github.com>
2020-07-21 17:41:36 +0900

    Feature / 5 / Login API 구현 (#37)
    * feat / 5 / login 기능 작성중
    
    * Core / server-setting
    
    * Core/Database Setting
    
    added get, post method in controllers
    
    * changed signin -> signup (#30)
    
    * feat : signup 기능 추가
    
    - migration에 createdAt, updatedAt default 값 추가
    - models는 프리티어 때문에 변경됨
    - models beforeCreate hooks 추가
    - signup 기능 추가
    
    * changed user_admission 1 -> 0
    
    * Core / package.json(mocha,chai...)
    
    * Feat / TestCase(signUp,EmailCheck)
    
    * Style business_number datatype(Integer->String)
    
     - User테이블에 business_number 컬럼을 Integer에서 String으로 데이터 타입 변경
    
    * add Fakedata
    
    * Add migration.js(charset,collate -> utf8)
    
    * Simply logging:false
    
    * Feat TC(signUp, login, signout, emailcheck)
    
    * fix / 6 / signup testcase clear
    
    * feat / 5 / login 기능 구현 완료
    
    * Rename server.test.js to user.test.js
    
    Co-authored-by: Daseul Sim <tlaektmf27@gmail.com>

--- controllers/user/login.js
+++ controllers/user/login.js
@@ -1,4 +1,30 @@
+const { users } = require("../../models");
+
 module.exports = {
-  get: () => {},
-  post: () => {},
+  post: (req, res) => {
+    let { email, password } = req.body;
+    users.findOne({ where: { email, password } }).then(data => {
+      if (data) {
+        if (data.user_admission === 1) {
+          req.session.userId = data.id;
+
+          res.status(201).send({
+            userInfo: {
+              username: data.username,
+              email: data.email,
+              phone: data.phone,
+              address: data.address,
+              user_type: data.user_type,
+            },
+          });
+        } else {
+          res
+            .status(201)
+            .send({ message: "아직 판매자 회원 승인을 받지 못했습니다." });
+        }
+      } else {
+        res.status(404).send({ message: "ID나 비밀번호가 일치하지 않습니다." });
+      }
+    });
+  },
 };
