99838002dfa8a4b6568b09e775ebd3750c1967f7
Daseul Sim <tlaektmf27@gmail.com>
2020-07-22 15:50:41 +0900

    Feat home API(recommendation, best)

--- controllers/home/index.js
+++ controllers/home/index.js
@@ -1,4 +1,50 @@
+const { goods } = require("../../models");
+
 module.exports = {
-  get: () => {},
-  post: () => {},
+  get: async (req, res) => {
+    let recommendationData = await goods
+      .findAll({
+        where: {},
+        offset: 0,
+        limit: 5,
+        order: [["stock"]],
+      })
+      .then((data) => {
+        return data.map((goods) => {
+          return {
+            id: goods.id,
+            title: `${goods.goods_name}에 대한 제목`,
+            contents: `${goods.goods_name}에 대한 내용`,
+            img: goods.goods_img,
+          };
+        });
+      });
+    let bestData = await goods
+      .findAll({
+        where: {},
+        offset: 0,
+        limit: 10,
+        order: [["stock"]],
+      })
+      .then((data) => {
+        return data.map((goods) => {
+          return {
+            id: goods.id,
+            title: `${goods.goods_name}에 대한 제목`,
+            price: goods.goods_price,
+            img: goods.goods_img,
+          };
+        });
+      });
+    if (recommendationData && bestData) {
+      res.status(201).send({
+        recommendation: recommendationData,
+        best: bestData,
+      });
+    } else {
+      res.status(404).send({
+        message: "data is not found",
+      });
+    }
+  },
 };
