0f61cc52b90ce5cc9659df918e098f44e2bb978d
Daseul Sim <tlaektmf27@gmail.com>
2020-07-23 13:31:22 +0900

    Feat editUserInfo frame

--- controllers/user/edituserinfo.js
+++ controllers/user/edituserinfo.js
@@ -1,4 +1,31 @@
+const { users } = require("../../models");
+const session = require("express-session");
 module.exports = {
-  get: () => {},
-  post: () => {},
+  post: (req, res) => {
+    let a = {
+      username: "판매자",
+      password: "1234",
+      phone: "010-1234-5678",
+      address: "서울시 OO구 OO로 OOO, 상세주소",
+      trade_name: "판매장",
+      business_number: "000-00-00000",
+    };
+    let session = req.sessoin.userId;
+    users
+      .findOne({
+        where: { id: session },
+        attribues: [
+          "username",
+          "password",
+          "phone",
+          "address",
+          "trade_name",
+          "business_number",
+        ],
+      })
+      .then((userData) => {
+        console.log(userData);
+        res.send();
+      });
+  },
 };
