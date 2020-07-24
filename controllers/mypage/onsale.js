752ee86a468e85a1847b4a9566d52a1e6724bdb5
Daseul Sim <tlaektmf27@gmail.com>
2020-07-24 16:25:23 +0900

    Feat onSale(등록한 상품) API

--- controllers/mypage/onsale.js
+++ controllers/mypage/onsale.js
@@ -1,4 +1,28 @@
+const { users, goods } = require("../../models");
 module.exports = {
-  get: () => {},
-  post: () => {},
+  get: (req, res) => {
+    const session = req.session.userId;
+    if (session) {
+      goods
+        .findAll({
+          where: {
+            user_id: session,
+          },
+          attributes: ["goods_name", "goods_img", "goods_price", "stock"],
+        })
+        .then((onSaleData) => {
+          if (onSaleData) {
+            res.status(200).send(onSaleData);
+          } else {
+            res.status(404).send({
+              message: "등록한 상품이 없습니다.",
+            });
+          }
+        });
+    } else {
+      res.status(404).send({
+        message: "세션이 존재하지 않습니다.",
+      });
+    }
+  },
 };
