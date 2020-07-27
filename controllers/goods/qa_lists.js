2c8e78cf9c9e16e1fe6dce960239ce2728658e28
Daseul Sim <tlaektmf27@gmail.com>
2020-07-27 18:35:36 +0900

    Feat qa_lists(GET) API

--- controllers/goods/qa_lists.js
+++ controllers/goods/qa_lists.js
@@ -1,15 +1,11 @@
-const { users, q_lists } = require("../../models");
+const { users, q_lists, reply } = require("../../models");
 
 module.exports = {
-  get: (req, res) => {
+  get: async (req, res) => {
     const { goods_id } = req.body;
     const session = req.session.userId;
-    if (!session) {
-      res.status(404).send({
-        message: "세션이 존재하지 않습니다.",
-      });
-    }
-    q_lists
+
+    let qLists = await q_lists
       .findAll({
         include: [
           {
@@ -23,8 +19,59 @@ module.exports = {
         attributes: ["id", "title", "contents"],
       })
       .then((qListsData) => {
-        console.log(qListsData);
+        if (qListsData.length === 0) {
+          res.status(404).send({
+            message: "질문이 존재하지 않습니다.",
+          });
+        } else {
+          let joinQLists = qListsData.map((qData) => {
+            return {
+              id: qData.id,
+              title: qData.title,
+              username: qData.user.username,
+              contents: qData.contents,
+            };
+          });
+          return joinQLists;
+        }
       });
+    if (qLists) {
+      let formmattedQList = [];
+      for (let i = 0; i < qLists.length; i++) {
+        let replies = await reply
+          .findAll({
+            include: [
+              {
+                model: users,
+                attributes: ["username"],
+              },
+            ],
+            where: {
+              q_lists_id: qLists[i].id,
+            },
+            attributes: ["text", "createdAt"],
+          })
+          .then((replyData) => {
+            let joinReply = replyData.map((reply) => {
+              return {
+                username: reply.user.username,
+                text: reply.text,
+                createdAt: reply.createdAt,
+              };
+            });
+            return joinReply;
+          });
+        formmattedQList.push({
+          id: qLists[i].id,
+          title: qLists[i].title,
+          username: qLists[i].username,
+          contents: qLists[i].contents,
+          reply: replies,
+        });
+      }
+
+      res.status(200).send(formmattedQList);
+    }
   },
   post: (req, res) => {
     const { goods_id, title, contents } = req.body;
