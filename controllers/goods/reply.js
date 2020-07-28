a983427885bef31a7af481f2b9dde8aa7d57534b
Daseul Sim <tlaektmf27@gmail.com>
2020-07-28 11:28:30 +0900

    Feat reply(Post) API

--- controllers/goods/reply.js
+++ controllers/goods/reply.js
@@ -1,4 +1,29 @@
+const { reply } = require("../../models");
 module.exports = {
-  get: () => {},
-  post: () => {},
+  post: (req, res) => {
+    const { text, qa_list_id } = req.body;
+    const session = req.session.userId;
+    if (!session) {
+      res.status(404).send({
+        message: "세션이 존재하지 않습니다.",
+      });
+    }
+    if (text) {
+      reply
+        .create({
+          user_id: session,
+          text: text,
+          q_lists_id: qa_list_id,
+        })
+        .then(() => {
+          res.status(201).send({
+            message: "리플 성공",
+          });
+        });
+    } else {
+      res.status(404).send({
+        message: "리플 실패",
+      });
+    }
+  },
 };
