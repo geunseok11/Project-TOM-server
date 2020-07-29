feff9ac2ea536e7cf2ac4c4465eeb4cb6c67f303
Daseul Sim <tlaektmf27@gmail.com>
2020-07-29 10:44:09 +0900

    Feat axios.get(trackingData)

--- controllers/mypage/tracking.js
+++ controllers/mypage/tracking.js
@@ -1,4 +1,28 @@
+const { order_lists } = require("../../models");
+const axios = require("axios");
+const { TRACKING_KEY } = require("../../config/config");
 module.exports = {
-  get: () => {},
-  post: () => {},
+  get: (req, res) => {
+    const session = req.session.userId;
+    if (!session) {
+      res.status(404).send({
+        message: "세션이 존재하지 않습니다.",
+      });
+    }
+    //======================================
+    axios
+      .get(
+        `https://info.sweettracker.co.kr/api/v1/trackingInfo?t_key=${TRACKING_KEY}&t_code=05&t_invoice=418173431690`
+      )
+      .then((resz) => {
+        console.log(Object.keys(resz));
+        console.log(resz.data);
+        res.send(resz.data);
+      })
+      .catch(() => {
+        res.status(404).send({
+          message: "없는 송장번호 입니다.",
+        });
+      });
+  },
 };
