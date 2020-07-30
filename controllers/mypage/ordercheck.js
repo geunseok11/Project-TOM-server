88f46f1c7e32910710a7ec4d451ceed3af901c9c
Daseul Sim <tlaektmf27@gmail.com>
2020-07-30 17:27:47 +0900

    Feat +case(order_lists=X, order_state != 0)

--- controllers/mypage/ordercheck.js
+++ controllers/mypage/ordercheck.js
@@ -10,30 +10,48 @@ module.exports = {
         message: "로그인이 필요한 서비스 입니다.",
       });
     }
-    axios
-      .get(
-        `https://info.sweettracker.co.kr/api/v1/trackingInfo?t_key=${TRACKING_KEY}&t_code=${delivery_company_id}&t_invoice=${invoice_number}`
-      )
-      .then((trackingData) => {
-        if (trackingData.data.msg) {
-          res.status(404).send({
-            message: "택배사코드나 송장번호를 제대로 입력해주세요.",
-          });
-        } else {
-          order_lists
-            .update(
-              {
-                invoice_number: invoice_number,
-                delivery_company_id: delivery_company_id,
-                order_state: 1,
-              },
-              { where: { id: order_lists_id } }
-            )
-            .then(() => {
-              res.status(200).send({
-                message: "정상적으로 입력 되었습니다.",
+    order_lists
+      .findOne({
+        where: order_lists_id,
+      })
+      .then((orderData) => {
+        if (orderData) {
+          if (orderData.order_state === 0) {
+            axios
+              .get(
+                `https://info.sweettracker.co.kr/api/v1/trackingInfo?t_key=${TRACKING_KEY}&t_code=${delivery_company_id}&t_invoice=${invoice_number}`
+              )
+              .then((trackingData) => {
+                if (trackingData.data.msg) {
+                  res.status(404).send({
+                    message: "택배사코드나 송장번호를 제대로 입력해주세요.",
+                  });
+                } else {
+                  order_lists
+                    .update(
+                      {
+                        invoice_number: invoice_number,
+                        delivery_company_id: delivery_company_id,
+                        order_state: 1,
+                      },
+                      { where: { id: order_lists_id } }
+                    )
+                    .then(() => {
+                      res.status(200).send({
+                        message: "정상적으로 입력 되었습니다.",
+                      });
+                    });
+                }
               });
+          } else {
+            res.status(404).send({
+              message: "이미 배송이 진행중이거나 완료된 상품입니다.",
             });
+          }
+        } else {
+          res.status(404).send({
+            message: "해당하는 판매내역이 없습니다.",
+          });
         }
       });
   },
